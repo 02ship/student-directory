e48a1d58147f1a5c5b030d9cea6c9e6f10f7e37d
Paul <paul.shields1@btinternet.com>
2020-07-03 17:03:45 +0000

    Added auto-load feature for loading student list

--- directory.rb
+++ directory.rb
@@ -2,7 +2,7 @@
 def interactive_menu
   loop do
     print_menu
-    process(gets.chomp)
+    process(STDIN.gets.chomp)
   end
 end
 
@@ -40,13 +40,13 @@ end
 def input_students
   puts "Please enter the name of the students"
   puts "To finish, just hit return twice"
-  name = gets.chomp
+  name = STDIN.gets.chomp
   #while the name returned is not empty
   while !name.empty? do
     @students << { name: name, cohort: :november }
     puts "Now we have #{@students.count} students"
     #gets another student's name
-    name = gets.chomp
+    name = STDIN.gets.chomp
   end
 end
 
@@ -74,13 +74,27 @@ def save_students
   file.close
 end
 
-def load_students
-  file = File.open("students.csv", "r")
+def load_students(filename = "students.csv")
+  file = File.open(filename, "r")
   file.readlines.each do |line|
     name, cohort = line.chomp.split(",")
     @students << { name: name, cohort: cohort.to_sym }
   end
   file.close
 end
+
+def try_load_students
+  filename = ARGV.first
+  return if filename.nil?
+  if File.exists?(filename)
+    load_students(filename)
+      puts "Loaded #{@students.count} from #{filename}"
+  else
+    puts "Sorry #{filename} doesn't exist"
+    exit
+  end
+end
+
+try_load_students
 interactive_menu
 
